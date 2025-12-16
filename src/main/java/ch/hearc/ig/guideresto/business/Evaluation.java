80f313260e1e1110663ba89c9754bf1fade5c7e0
jonasraval <jonas.raval@he-arc.ch>
2025-12-16 09:20:31 +0100

    fix : GeneratedValue duplicate

--- src/main/java/ch/hearc/ig/guideresto/business/Evaluation.java
+++ src/main/java/ch/hearc/ig/guideresto/business/Evaluation.java
@@ -21,9 +21,6 @@ public abstract class Evaluation implements IBusinessObject {
             sequenceName = "SEQ_EVAL",
             allocationSize = 1
     )
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-            generator = "SEQ_EVAL"
-    )
     private Integer id;
 
     @Column(name="DATE_EVAL", nullable=false)
